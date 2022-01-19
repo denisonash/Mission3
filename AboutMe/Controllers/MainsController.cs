a0017686cc9a37d591f46785a202d0d66f7f44cd
ashleydenison <dashley9@byu.edu>
2022-01-19 14:00:40 -0700

    Final commit
    Final Commits

---
+++ AboutMe/Controllers/MainsController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AboutMe.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AboutMe.Controllers
+{
+    public class MainsController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Calc()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calc(CalcModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ AboutMe/Models/CalcModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AboutMe.Models
+{
+    public class CalcModel
+    {
+        //Format:
+        //Validator for required field
+        //validator for 0 to 100
+        //Create the get and set for input variables
+
+        [Required]
+        [Range(0,100)]
+        public byte Assign { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public byte GP { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public byte Quiz { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public byte Exam { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public byte Intex { get; set; }
+    }
+}
